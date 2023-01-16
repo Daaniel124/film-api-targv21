ecee64524f730290f3ef61392cf284f4386db1d4
daaniel124 <daaniel124@gmail.com>
2023-01-16 17:16:55 +0200

    add mariadb connection

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 require("dotenv").config()
 const app = require("express")()
-const mysql = require("mysql")
+//const mysql = require("mysql")
+const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_BASE
-})
+})*/
 
-connection.connect(function(err){
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_BASE,
+    connectionLimit: 5
+})
+/*connection.connect(function(err){
     if(err) throw err
     connection.query("SELECT * FROM customers", 
     function(err, result, fields){
         if(err) throw err
         console.log(result)
     })
+})*/
+
+app.get("/customers", async (req,res)=>{
+    let conn
+    try {
+        conn = await pool.getConnection()
+        const rows = await conn.query("SELECT firstname, lastname FROM customers")
+        res.send(JSON.stringify(rows))
+    } catch (error) {
+        console.log(error)
+    } finally{
+        conn.end()
+    }
 })
 
 app.listen(port,()=>{
