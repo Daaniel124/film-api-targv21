3d0f0fc5934f7d3c7c1b8e41b602b793118dd3e1
daaniel124 <daaniel124@gmail.com>
2023-01-23 17:29:41 +0200

    Messages for films

--- controllers/FilmsController.js
+++ controllers/FilmsController.js
@@ -5,3 +5,15 @@ exports.getAll = async (req,res)=>{
     const films = await Films.findAll({attributes:["id", "title"]})
     res.send(JSON.stringify(films))
 }
+exports.createNew = async (req,res)=>{
+    res.send({"message":"Not ipmlemented yet"})
+}
+exports.getById = async (req,res)=>{
+    res.send({"message":"Not ipmlemented yet"})
+}
+exports.updateById = async (req,res)=>{
+    res.send({"message":"Not ipmlemented yet"})
+}
+exports.deleteById = async (req,res)=>{
+    res.send({"message":"Not ipmlemented yet"})
+}
