2f42fbe31465a3cd8fc99e4862716956c2af6ecc
daaniel124 <daaniel124@gmail.com>
2023-02-06 14:11:45 +0200

    Add update function [#184401350]

--- controllers/FilmsController.js
+++ controllers/FilmsController.js
@@ -37,7 +37,29 @@ exports.getById = async (req,res)=>{
     }
 }
 exports.updateById = async (req,res)=>{
-    res.send({"message":"Not ipmlemented yet"})
+    let film = await Films.findByPk(req.params.id, {logging: console.log})
+    if (film === null) {
+        res.status(404).send({"error":"Film not found"})
+        return
+    }  
+    try {
+        film = await film.update(req.body,
+        {
+            where: { id: req.params.id},
+            logging:console.log
+        })
+    } catch (error) {
+        if (error instanceof db.Sequilize.ValidationError) {
+            res.status(400).send({"error":error.errors.map((item)=> item.message)})
+        } else {
+            console.log("FilmsUpdate:",error)
+            res.status(500).send({"error":"Something went wrong on our side. Sorry :("})
+        }
+        return 
+    }
+    res.status(200)
+        .location(`${getBaseUrl(req)}/films/${film.id}`)
+        .json(film)
 }
 exports.deleteById = async (req,res)=>{
     res.send({"message":"Not ipmlemented yet"})
--- routers/app.routers.js
+++ routers/app.routers.js
@@ -4,26 +4,26 @@ const sessionsController = require("../controllers/SessionsController.js")
 
 module.exports = (app)=>{
     app.route("/films")
-    .get(filmsController.getAll)
-    .post(filmsController.createNew)    //Create
+        .get(filmsController.getAll)
+        .post(filmsController.createNew)    //Create
     app.route("/films/:id")
-    .get(filmsController.getById)       //Read
-    .put(filmsController.updateById)    //Update
-    .delete(filmsController.deleteById) //Delete
+        .get(filmsController.getById)       //Read
+        .put(filmsController.updateById)    //Update
+        .delete(filmsController.deleteById) //Delete
 
     app.route("/tickets")
-    .get(ticketsController.getAll)
-    .post(ticketsController.createNew)    //Create
+        .get(ticketsController.getAll)
+        .post(ticketsController.createNew)    //Create
     app.route("/tickets/:id")
-    .get(ticketsController.getById)       //Read
-    .put(ticketsController.updateById)    //Update
-    .delete(ticketsController.deleteById) //Delete
+        .get(ticketsController.getById)       //Read
+        .put(ticketsController.updateById)    //Update
+        .delete(ticketsController.deleteById) //Delete
 
     app.route("/sessions")
-    .get(sessionsController.getAll)
-    .post(sessionsController.createNew)    //Create
+        .get(sessionsController.getAll)
+        .post(sessionsController.createNew)    //Create
     app.route("/sessions/:id")
-    .get(sessionsController.getById)       //Read
-    .put(sessionsController.updateById)    //Update
-    .delete(sessionsController.deleteById) //Delete
+        .get(sessionsController.getById)       //Read
+        .put(sessionsController.updateById)    //Update
+        .delete(sessionsController.deleteById) //Delete
 }
