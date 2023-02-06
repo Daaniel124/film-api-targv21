670c8a0ec56ba300295a6fadd1c8c7b19e4a3401
daaniel124 <daaniel124@gmail.com>
2023-02-06 17:26:35 +0200

    Add new function getAll

--- controllers/TicketsController.js
+++ controllers/TicketsController.js
@@ -2,13 +2,29 @@ const { db } = require("../db");
 const { getBaseUrl } = require("./helpers")
 const Tickets = db.tickets
 
-exports.getAll = async (req,res)=>{
+/*exports.getAll = async (req,res)=>{
     const tickets = await Tickets.findAll({attributes:["id", "firstName"]})
     if (tickets.length == 0) {
         res.send({"message":"No tickets exist"}).end()
     } else {
         res.send(JSON.stringify(tickets))
     }
+}*/
+
+exports.getAll = async (req,res) =>{
+    const tickets = await Tickets.findAll({
+        include: { all: true},
+        logging: console.log
+    })
+    /*let result = []
+    result = tickets.map( (gp) => {
+        return {
+            "gameName": gp.Game.name,
+            "playerName": `${gp.Player.firstName} ${gp.Player.lastName}`,
+            "playTime": gp.playTimeMinutes
+        }
+    })
+    res.send(result)*/
 }
 
 exports.createNew = async (req,res)=>{
