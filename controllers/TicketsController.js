23a848c345f5cb0bd01dd59f0b36977ca24a9c00
daaniel124 <daaniel124@gmail.com>
2023-02-06 15:41:40 +0200

    Add getByID function in tickets [#184237766]

--- controllers/TicketsController.js
+++ controllers/TicketsController.js
@@ -1,4 +1,5 @@
 const { db } = require("../db");
+const { getBaseUrl } = require("./helpers")
 const Tickets = db.tickets
 
 exports.getAll = async (req,res)=>{
@@ -14,7 +15,12 @@ exports.createNew = async (req,res)=>{
     res.send({"message":"Not ipmlemented yet"})
 }
 exports.getById = async (req,res)=>{
-    res.send({"message":"Not ipmlemented yet"})
+    const ticket = await Tickets.findByPk(req.params.id, {logging: console.log})
+    if (ticket === null) {
+        res.status(404).send({"error":"Ticket not found"})
+    } else {
+        res.send(ticket)
+    }
 }
 exports.updateById = async (req,res)=>{
     res.send({"message":"Not ipmlemented yet"})
